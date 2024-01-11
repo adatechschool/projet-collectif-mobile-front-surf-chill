867e05aabf5e35f2605dad2e70caffb45d7a7137
alexisc <a.chentreu@protonmail.com>
2024-01-11 15:06:22 +0100

    Image displayed

--- app/src/main/java/com/example/surfchill/MainActivity.kt
+++ app/src/main/java/com/example/surfchill/MainActivity.kt
@@ -9,8 +9,20 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.surfchill.ui.theme.SurfChillTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +34,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingImage(
+                        stringResource(R.string.app_name),
+                    )
                 }
             }
         }
@@ -30,17 +44,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 48.sp,
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun GreetingPreview() {
     SurfChillTheme {
-        Greeting("Android")
+        GreetingImage(stringResource(R.string.app_name))
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, modifier: Modifier = Modifier) {
+    val image= painterResource(R.drawable.bud_bud_vendee)
+    Box {
+        Image(painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.9f
+        )
     }
 }
