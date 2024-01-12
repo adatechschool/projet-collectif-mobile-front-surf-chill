3fabd15254fe5f3eefbdc67d3f401138125c5b9e
LaureSaintAntonin <lsaint_antonin@yahoo.fr>
2024-01-12 09:54:24 +0100

    image full screen + titre qui s'affiche

--- app/src/main/java/com/example/surfchill/MainActivity.kt
+++ app/src/main/java/com/example/surfchill/MainActivity.kt
@@ -13,16 +13,15 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.surfchill.ui.theme.SurfChillTheme
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,9 +33,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(
-                        stringResource(R.string.app_name),
-                    )
+                    HomeScreen() //(stringResource(R.string.app_name),
+
                 }
             }
         }
@@ -44,38 +42,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun HomeScreen(modifier: Modifier = Modifier) {
     Column(
-        verticalArrangement = Arrangement.Center,
         modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp)
     ) {
-        Text(
-            text = message,
-            fontSize = 48.sp,
-            lineHeight = 100.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(8.dp)
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun GreetingPreview() {
-    SurfChillTheme {
-        GreetingImage(stringResource(R.string.app_name))
-    }
-}
-
-@Composable
-fun GreetingImage(message: String, modifier: Modifier = Modifier) {
-    val image= painterResource(R.drawable.bud_bud_vendee)
-    Box {
-        Image(painter = image,
+        Image(
+            painter = painterResource(id= R.drawable.bud_bud_vendee),
             contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
             contentScale = ContentScale.Crop,
-            alpha = 0.9f
-        )
+            alpha = 0.9F
+        )}}
+
+@Composable
+fun TitleApp(message: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Surf and Chill",
+            fontSize = 40.sp,
+            lineHeight = 66.sp,
+            textAlign = TextAlign.Center,
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )}
+
+//@Composable
+//fun ButtonGo(){
+        //Button(
+            //onClick = { /* Handle button click */ },
+            //modifier = Modifier
+                //.fillMaxWidth()
+                //.height(50.dp)
+        //)
+//}
+
+//@Composable
+//fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    //Column(
+        //verticalArrangement = Arrangement.Center,
+        //modifier = modifier.padding(8.dp)
+    //) {
+        //Text(
+            //text = message,
+            //fontSize = 40.sp,
+            //lineHeight = 66.sp,
+            //textAlign = TextAlign.Center,
+            //modifier = Modifier
+        //)
+    //}
+//}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    SurfChillTheme {
+        HomeScreen()
+        TitleApp(stringResource(id = R.string.app_name))
     }
 }
